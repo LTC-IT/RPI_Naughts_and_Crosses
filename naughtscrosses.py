95d751449cebd8569510b24fe332f3e510c50246
Ryan Cather <LTCPcgames@gmail.com>
2019-03-26 10:24:29 +1100

    Added docstrings and comments Ignored user selection if cell was already selected

--- naughtscrosses.py
+++ naughtscrosses.py
@@ -1,23 +1,41 @@
 from sense_emu import SenseHat
 
 sense = SenseHat()
-player_one = True
+player_one = True  # Flag to keep track of whose turn it is.
 
 '''
 TODO
 Problems to solve - no particular order
 1 - //Move between "cells"
-2 - Select a cell (not an existing cells)
-3 - Indicate which is X and which is O
-4 - swap between players
+2 - //Select a cell (not an existing cells)
+3 - //Indicate which is X and which is O
+4 - //swap between players
+4a - //disallow selecting cells already selected.
 5 - Detect "win"
 6 - Detect "loss" when drawn.
+7 - reset the game
 '''
 
 def draw_hash():
-    b = [0,0,0]
-    w = [255,255,255]
+    """
+    Draws the # on the Raspberry Pi Sense Hat in white pixels,
+    leaving the others black.
+    The # is the 'board' for naughts and crosses.
+
+    Parameters:
+    None
+    E.g. id (int): An int for the 'cell' of the naughts and crosses board
+
+    Returns:
+    None
+    E.g. student_name: a string with the firstname + ' ' + secondname
+    """
     
+    # sets black and white
+    b = [0,0,0]            # black
+    w = [255,255,255]      # white
+    
+    # grid of 64 leds, black or white, to show the # board.
     grid = [
     b,b,w,b,b,w,b,b,    
     b,b,w,b,b,w,b,b,
@@ -29,12 +47,28 @@ def draw_hash():
     b,b,w,b,b,w,b,b
     ]
     
-    sense.set_pixels(grid)
+    sense.set_pixels(grid) # draws the grid on the sense hat leds
     
 def highlight_cell(id):
-    g = [0,255,0]
-    b = [0,0,0]
-    w = [255,255,255]
+    """
+    On each 'refresh' of the sense hat led matrix, redraws the game screen.
+    Highlights the cell (green) that the user is scrolling through using the sense hat joystick.
+    If player 1 has selected a cell, highlight that cell red
+    If player 2 has selected a cell, highlight that cell blue
+    Redraws the grid as white #.
+    
+    All other cells are left as black.
+
+    Parameters:
+    id (int): The value of the cell that the user is currently highlighting.
+
+    Returns:
+    None
+
+    """
+    g = [0,255,0]        # green is the highlight colour
+    b = [0,0,0]          # black is the colour when cells are not highlighted or selected
+    w = [255,255,255]    # White is for the grid #
     r = [255,0,0]        # Player 1's colour
     bl = [0,0,255]       # Player 2's colour
     
@@ -52,8 +86,14 @@ def highlight_cell(id):
     #These are the indexes of the LEDs in grid that need to be white for the hash
     hash = [2,5,10,13,16,17,18,19,20,21,22,23,26,29,34,37,40,41,42,43,44,45,46,47,50,53,58,61]
     
-    starting_pixels = [0,3,6,24,27,30,48,51,54]
+    starting_pixels = [0,3,6,24,27,30,48,51,54] # TODO: what does this do?
     
+    '''
+    This loop iterates over the game_grid.
+    If the value is 1, set the cell as blue
+    If the value is 2, set the cell as red
+    Uses the starting pixel to set the four pixels as the intended colour
+    '''
     for index in range (0,9):
         starting_pixel = starting_pixels[index]
         if game_grid[index] == 1:
@@ -68,25 +108,38 @@ def highlight_cell(id):
         grid[starting_pixel+9] = colour 
         
     
+    # This sets the cell the user is highlighting to be green
     starting_pixel = starting_pixels[id]
     
     grid[starting_pixel] = g
-    grid[starting_pixel+1] = g
-    grid[starting_pixel+8] = g
+    #grid[starting_pixel+1] = g
+    #grid[starting_pixel+8] = g
     grid[starting_pixel+9] = g
     
     
+    # Draws the white # game board
+    # and replaces any remaining 0's in grid to be black
+    # This for loop is to be the last code run in this function.
+    # because it captures any remaining 0's in grid and makes it black.
     for index in range (0,64):
         if index in hash:
             grid[index] = w
         if grid[index] == 0:
             grid[index] = b
-    
-    #print(grid)
+
     
     sense.set_pixels(grid)
 
 def initialise_game_grid():
+    """Creates the game grid list to represent the cells that players have selected or not.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+
+   """
     global game_grid
     # 0 = not selected
     # 1 = O selected
@@ -95,6 +148,17 @@ def initialise_game_grid():
     game_grid = [0,0,0,0,0,0,0,0,0]
     
 def select_cells():
+    """The user uses the joystick on the sense hat api to scroll through the cells before chosing one
+    As the user presses right, it calls the highlight_cell function to change the cell to green.
+    If the user chooses a cell, it updates the game grid to indicate which user chose it.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+
+   """
     global player_one
     '''
     ids of the cells
@@ -118,11 +182,13 @@ def select_cells():
                 # store the id of the cell selected
                 
                 if player_one:
-                    game_grid[current_id] = 1
-                    player_one = False
+                    if game_grid[current_id] == 0:
+                        game_grid[current_id] = 1
+                        player_one = False
                 else:
-                    game_grid[current_id] = 2
-                    player_one = True
+                    if game_grid[current_id] == 0:
+                        game_grid[current_id] = 2
+                        player_one = True
                 
                 print(game_grid)
             
@@ -134,54 +200,4 @@ def select_cells():
 initialise_game_grid()
 draw_hash()
 select_cells()
-
-
-'''
-if id == 0:
-        grid[0] = g
-        grid[1] = g
-        grid[8] = g
-        grid[9] = g
-    if id == 1:
-        grid[3] = g
-        grid[4] = g
-        grid[11] = g
-        grid[12] = g
-    if id == 2:
-        grid[6] = g
-        grid[7] = g
-        grid[14] = g
-        grid[15] = g
-    if id == 3:
-        grid[24] = g
-        grid[25] = g
-        grid[32] = g
-        grid[33] = g
-    if id == 4:
-        grid[27] = g
-        grid[28] = g
-        grid[35] = g
-        grid[36] = g
-    if id == 5:
-        grid[30] = g
-        grid[31] = g
-        grid[38] = g
-        grid[39] = g
-    if id == 6:
-        grid[48] = g
-        grid[49] = g
-        grid[56] = g
-        grid[57] = g
-    if id == 7:
-        grid[51] = g
-        grid[52] = g
-        grid[59] = g
-        grid[60] = g
-    if id == 8:
-        grid[54] = g
-        grid[55] = g
-        grid[62] = g
-        grid[63] = g
-'''
-
 
